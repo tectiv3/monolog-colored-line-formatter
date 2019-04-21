6e03541443ba2bcf11d506daad544994003e70aa
tectiv3 <git@getpanda.me>
2019-04-21 15:59:59 +0900

    Add ability to limit stack trace depth

--- src/Formatter/ColoredLineFormatter.php
+++ src/Formatter/ColoredLineFormatter.php
@@ -3,6 +3,7 @@
 namespace Bramus\Monolog\Formatter;
 
 use Bramus\Monolog\Formatter\ColorSchemes\ColorSchemeInterface;
+use \Monolog\Utils;
 
 /**
  * A Colored Line Formatter for Monolog
@@ -14,6 +15,12 @@ class ColoredLineFormatter extends \Monolog\Formatter\LineFormatter
      * @var ColorSchemeInterface
      */
     private $colorScheme = null;
+
+    /**
+     * Limit stack trace depth
+     * @var int
+     */
+    private $stackLimit = null;
 
     /**
      * @param string $format                     The format of the message
@@ -53,14 +60,117 @@ class ColoredLineFormatter extends \Monolog\Formatter\LineFormatter
     }
 
     /**
+     * Sets Stack Trace Limit
+     * @param int
+     */
+    public function setStackLimit(int $stackLimit)
+    {
+        $this->stackLimit = $stackLimit;
+    }
+
+    /**
      * {@inheritdoc}
      */
-    public function format(array $record) : string
+    public function format(array $record): string
     {
         // Get the Color Scheme
         $colorScheme = $this->getColorScheme();
 
         // Let the parent class to the formatting, yet wrap it in the color linked to the level
-        return $colorScheme->getColorizeString($record['level']).trim(parent::format($record)).$colorScheme->getResetString()."\n";
+        return $colorScheme->getColorizeString($record['level']) . trim(parent::format($record)) . $colorScheme->getResetString() . "\n";
+    }
+
+    private function formatException(\Throwable $e): string
+    {
+        $str = '[object] (' . Utils::getClass($e) . '(code: ' . $e->getCode();
+        if ($e instanceof \SoapFault) {
+            if (isset($e->faultcode)) {
+                $str .= ' faultcode: ' . $e->faultcode;
+            }
+            if (isset($e->faultactor)) {
+                $str .= ' faultactor: ' . $e->faultactor;
+            }
+            if (isset($e->detail)) {
+                $str .= ' detail: ' . $e->detail;
+            }
+        }
+        $str .= '): ' . $e->getMessage() . ' at ' . $e->getFile() . ':' . $e->getLine() . ')';
+        if ($this->includeStacktraces) {
+            $str .= "\n[stacktrace]\n" . $this->getTraceAsString($e) . "\n";
+        }
+        return $str;
+    }
+
+    public function getTraceAsString(\Throwable $e): string
+    {
+        $str = '';
+        $trace = $e->getTrace();
+        if (!is_array($trace)) {
+            return $str;
+        }
+        foreach ($trace as $index => $frame) {
+            if ($this->stackLimit && $index >= $this->stackLimit) {
+                break;
+            }
+            if (!is_array($frame)) {
+                continue;
+            }
+            $str .= $this->_build_trace_string($frame, $index);
+        }
+        return $str;
+    }
+
+    private function _build_trace_string($frame, $index): string
+    {
+        $str = "#${index} ";
+
+        $file = $frame['file'];
+        if ($file) {
+            if (!is_string($file)) {
+                $str .= "[unknown function] ";
+            } else {
+                $line = $frame['line'];
+                $str .= $file . ':' . $line . ' ';
+            }
+        } else {
+            $str .= '[internal function] ';
+        }
+        $str .= $frame['class'];
+        $str .= $frame['type'];
+        $str .= $frame['function'] . '(';
+        $args = $frame['args'];
+        if (is_array($args)) {
+            $last_arg = count($args) - 1;
+            foreach ($args as $key => $value) {
+                $str .= $this->_build_trace_args($value);
+                if ($key != $last_arg) {
+                    $str .= ', ';
+                }
+            }
+        }
+        $str .= ")\n";
+        return $str;
+    }
+
+    private function _build_trace_args($arg): string
+    {
+        switch (gettype($arg)) {
+            case "boolean":
+                return $arg ? 'true' : 'false';
+            case "integer":
+                return $arg;
+            case "double":
+                return sprintf("%.2f", $arg);
+            case "string":
+                return substr($arg, 0, 15) . strlen($arg) > 15 ? '...' : '';
+            case "object":
+                return 'Object(' . get_class($arg) . ')';
+            case "array":
+            case "resource":
+            case "resource (closed)":
+            case "NULL":
+                return gettype($arg);
+        }
+        return "unknown type";
     }
 }
